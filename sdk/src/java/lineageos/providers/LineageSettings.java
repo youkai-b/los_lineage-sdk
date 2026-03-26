166168b35db0d229dd60f8c682051027e9b36d96
grepfox <grepfox@tutamail.com>
2026-03-26 22:56:04 +0530

    sdk: Add an option to take screenshot using hw keys
    Change-Id: I4a979f38977023abf2380ef77cadfa463cc46630

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -385,7 +385,7 @@ public final class LineageSettings {
             new InclusiveIntegerRangeValidator(0, 86400);
 
     private static final Validator sHardwareKeyActionvalidator =
-            new InclusiveIntegerRangeValidator(0, 10);
+            new InclusiveIntegerRangeValidator(0, 11);
 
     private static final Validator sAlwaysTrueValidator = new Validator() {
         @Override
@@ -1098,6 +1098,7 @@ public final class LineageSettings {
          * 8 - Last app
          * 9 - Toggle split screen
          * 10 - Kill foreground app
+         * 11 - Take screenshot
          */
         public static final String KEY_HOME_LONG_PRESS_ACTION = "key_home_long_press_action";
 
